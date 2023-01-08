83f343b04d621567a0b8e3e4fa703ed52a1b0d7e
muhammadreza marvi <muhdrmv@gmail.com>
2023-01-08 16:04:29 +0330

    asdsa

--- controllers/rdp-prepare.js
+++ controllers/rdp-prepare.js
@@ -1,6 +1,5 @@
 
 module.exports = async (ip, port) => {
-
      let str = `screen mode id:i:2\nuse multimon:i:0\ndesktopwidth:i:1920\ndesktopheight:i:1080\nsession bpp:i:32\nwinposstr:s:0,1,1796,69,2643,570\ncompression:i:1\nkeyboardhook:i:2\naudiocapturemode:i:0\nvideoplaybackmode:i:1\nconnection type:i:7\nnetworkautodetect:i:1\nbandwidthautodetect:i:1\ndisplayconnectionbar:i:1\nenableworkspacereconnect:i:0\ndisable wallpaper:i:0\nallow font smoothing:i:0\nallow desktop composition:i:0\ndisable full window drag:i:1\ndisable menu anims:i:1\ndisable themes:i:0\ndisable cursor setting:i:0\nbitmapcachepersistenable:i:1\nfull address:s:${ip}:${port}\naudiomode:i:0\nredirectprinters:i:1\nredirectcomports:i:0\nredirectsmartcards:i:1\nredirectclipboard:i:1\nredirectposdevices:i:0\nautoreconnection enabled:i:1\nauthentication level:i:2\nprompt for credentials:i:0\nnegotiate security layer:i:1\nremoteapplicationmode:i:0\nalternate shell:s:\nshell working directory:s:\ngatewayhostname:s:\ngatewayusagemethod:i:4\ngatewaycredentialssource:i:4\ngatewayprofileusagemethod:i:0\npromptcredentialonce:i:0\ngatewaybrokeringtype:i:0\nuse redirection server name:i:0\nrdgiskdcproxy:i:0\nkdcproxyname:s:\nredirectwebauthn:i:1\nenablerdsaadauth:i:0`;
-     return str
+     return str;
 }
--- routes/sessions.js
+++ routes/sessions.js
@@ -38,6 +38,8 @@ router.get('/creating-session', async function(req, res, next) {
   // let {
   //   pam_session_uuid, user_id, ip, meta
   // } = req.body;
+  // if(!pam_session_uuid || !user_id || !ip || !meta) return;
+
 
   let id = v1(uuidv1)
       pam_session_id  = "7b78eab6-46f6-45e6-b9b6-3bdfd2260f5b",
@@ -46,9 +48,9 @@ router.get('/creating-session', async function(req, res, next) {
 
   let port_available = false;
   let port = null;
-  console.log(id);
+
   while (!port_available) {
-    port = randomIntBetween(10000,90000)
+    port = randomIntBetween(10000,65535)
     let res_repetetive_port = await check_repetitive_port(port);
     if(!res_repetetive_port) port_available = true
   }
@@ -65,18 +67,72 @@ router.get('/creating-session', async function(req, res, next) {
 
   insert_session(sessionInput)
 
-  exec(`docker run -v $PWD/pyrdp_output:/home/pyrdp/pyrdp_output -p ${port}:3389 ${process.env.TRANSPARENT} pyrdp-mitm.py ${ip}`, (err, stdout, stderr) => {
+  exec(`docker run -d --name ${pam_session_id} -v /root/rjpn/${process.env.TRANSPARENT_VERSION_FOLDER}/pyrdp_output:/home/pyrdp/pyrdp_output -p ${port}:3389 ${process.env.TRANSPARENT} pyrdp-mitm.py -si ${pam_session_id} ${ip}`, (err, stdout, stderr) => {
     if (err) return;
   });
 
-  let str = await rdp_prepare(ip, port)
+  let str = await rdp_prepare(process.env.IP, port)
 
-  let filname = `${ip}_${port}_${id}.rdp`;
+  let filname = `${ip}_${port}_${pam_session_id}.rdp`;
 
-  fs.writeFile(`./connections/${filname}`, str, (err) => {
+  fs.writeFile(`${process.env.RDP_CONNECTION_PATH}/${filname}`, str, (err) => {
 
     if (err) throw err;
-    res.download(`./connections/${filname}`)
+    res.download(`${process.env.RDP_CONNECTION_PATH}/${filname}`)
+  });
+
+});
+
+router.get('/terminate-session', async function(req, res, next) {
+
+  // let {
+  //   pam_session_uuid
+  // } = req.body; 
+  // if(!pam_session_id) return;
+
+  let pam_session_id = "muhdrmv";
+
+  exec(`docker rm -f ${pam_session_id}`, (err, stdout, stderr) => {
+
+    if (err) return err;
+    if(stdout != pam_session_id) return;
+
+    // Action:  update session id (Live => closed)
+  });  
+});
+
+router.get('/export-keystrokes', async function(req, res, next) {
+
+  // let {
+  //   pam_session_uuid
+  // } = req.body; 
+  // if(!pam_session_id) return;
+
+  exec(`docker run -v /root/rjpn/${process.env.TRANSPARENT_VERSION_FOLDER}/pyrdp_output:/home/pyrdp/pyrdp_output ${process.env.TRANSPARENT} pyrdp-player.py --headless pyrdp_output/replays/${pam_session_id}.pyrdp`, (err, stdout, stderr) => {
+
+    if (err) return;
+    console.log(stdout);
+
+    fs.writeFile(`${process.env.KEYSTROKES_PATH}/${pam_session_id}.txt`, stdout, (err) => {
+      if (err) throw err;
+      res.download(`${process.env.KEYSTROKES_PATH}/${pam_session_id}.txt`)
+    });
+
+  });
+
+});
+
+router.get('/export-video', async function(req, res, next) {
+
+  // let {
+  //   pam_session_uuid
+  // } = req.body; 
+  // if(!pam_session_id) return;
+
+  exec(`docker run -v /root/rjpn/${process.env.TRANSPARENT_VERSION_FOLDER}/pyrdp_output:/home/pyrdp/pyrdp_output ${process.env.TRANSPARENT} pyrdp-player.py --headless pyrdp_output/replays/${pam_session_id}.pyrdp`, (err, stdout, stderr) => {
+
+    if (err) return;
+    console.log(stdout);
   });
 
 });
